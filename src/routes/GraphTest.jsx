00c5e5311d35eeaa7521cd0fc0fdc32bfbcff327
JuanmiBassa <juanmi7633@gmail.com>
2024-02-21 16:40:38 +0100

    estilos y grafica

--- src/routes/GraphTest.jsx
+++ src/routes/GraphTest.jsx
@@ -1,14 +1,32 @@
 import '../styles/graphTest.css'
 import { useTranslation } from 'react-i18next'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import generateGraph from '../scripts/graphGenerator.js';
 
 export default function GraphTest() {
     const { t } = useTranslation();
     const [selectedValue, setSelectedValue] = useState('');
 
-  const handleInputChange = (event) => {
-    setSelectedValue(event.target.value);
-  };
+    const handleInputChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
+    useEffect(() => {
+        const dataGraph1 = {
+            canvasProps: {
+                id: 'canvas1',
+                maxWidth: 500,
+                height: 200,
+            },
+            values: [40, 80, 70, 50, 100, 40, 92, 70, 120],
+            color: '#FB5350',
+            scaleMarks: 5,
+            limitNums: 10,
+            circles: false,
+        }
+
+        generateGraph(dataGraph1);
+    }, []);
 
     return (
         <>
@@ -27,15 +45,21 @@ export default function GraphTest() {
 
                 <div className="radio-input">
                     <label htmlFor=""></label>
-                    <input name="style-radio" type="radio" className="graph-style" 
-                    value="1" onChange={handleInputChange}/>
-                    <input name="style-radio" type="radio" className="graph-style" 
-                    value="2" onChange={handleInputChange}/>
-                    <input name="style-radio" type="radio" className="graph-style" 
-                    value="3" onChange={handleInputChange}/>
+                    <input name="style-radio" type="radio" className="graph-style"
+                        value="1" onChange={handleInputChange} />
+                    <input name="style-radio" type="radio" className="graph-style"
+                        value="2" onChange={handleInputChange} />
+                    <input name="style-radio" type="radio" className="graph-style"
+                        value="3" onChange={handleInputChange} />
                 </div>
                 <p>Selected Value: {selectedValue}</p>
 
+            </section>
+
+            <section>
+                <div className="graph_container">
+                    <canvas id="canvas1"></canvas>
+                </div>
             </section>
         </>
     )
